9f6cddad8b0f2f3550c7a1418e799e10b137a238
SinedGG <denis2320032@gmail.com>
2022-01-07 22:44:31 +0200

    Created queue control module ✨

---
+++ modules/queueControl.js
@@ -0,0 +1,17 @@
+module.exports = {
+  prew: function (message, servers) {
+    var server = servers[message.guild.id];
+    if (server.queue.position - 1 < 0) return;
+    servers[message.guild.id].queue.position-=2;
+    if (server.dispatcher) server.dispatcher.end();
+  },
+
+  skip: function (message, servers) {
+    var server = servers[message.guild.id];
+    if (server.dispatcher) server.dispatcher.end();
+  },
+
+  stop: function (message, servers) {
+    servers[message.guild.id].connection.disconnect();
+  }
+};
