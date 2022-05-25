abe5d3ffcd44ee1fa19a9ea445c15c55e2e5b2f1
SinedGG <denis2320032@gmail.com>
2022-05-25 17:58:16 +0300

    ✨ | Add error handling

--- modules/queueControl.js
+++ modules/queueControl.js
@@ -8,12 +8,25 @@ module.exports = {
 
   skip: function (message, servers) {
     var server = servers[message.guild.id];
+    var args = message.content.split(" ");
+    if (args[1]) {
+      var parsed = parseInt(args[1]);
+      if (!isNaN(parsed)) {
+        servers[message.guild.id].queue.position += parsed;
+      }
+    }
     if (server.dispatcher) server.dispatcher.stop();
   },
 
   stop: function (message, servers) {
     var server = servers[message.guild.id];
-    if (server.last_message) server.last_message.reactions.removeAll();
+    if (server.last_message)
+      server.last_message.reactions.removeAll().catch((error) => {});
     server.connection.disconnect();
+  },
+
+  add: function (server, url, author) {
+    server.queue.url.push(url);
+    server.queue.requested.push(author);
   },
 };
